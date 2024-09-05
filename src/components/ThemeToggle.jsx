1dd4e0406db18a0fa15a7baccf2dda1819ee9e7f
Megagig <megagigsoftwaresolutions.com>
2024-09-05 15:14:33 +0100

    Add the AppContext

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { BsFillSunFill, BsFillMoonFill } from 'react-icons/bs';
+import { useAppContext } from '../context/AppContext';
 
 const ThemeToggle = () => {
+  const { isDarkTheme, toggleDarkTheme } = useAppContext();
   return (
-    <div>
-      <h2>ThemeToggle</h2>
-    </div>
+    <section className="toggle-container">
+      <button className="dark-toggle" onClick={toggleDarkTheme}>
+        {isDarkTheme ? (
+          <BsFillSunFill className="toggle-icon" />
+        ) : (
+          <BsFillMoonFill className="toggle-icon" />
+        )}
+      </button>
+    </section>
   );
 };
 
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -3,8 +3,15 @@ import { useContext, useState, useEffect, createContext } from 'react';
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+  };
   return (
-    <AppContext.Provider value={{ children }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ isDarkTheme, toggleDarkTheme }}>
+      {children}
+    </AppContext.Provider>
   );
 };
 export const useAppContext = () => useContext(AppContext);
