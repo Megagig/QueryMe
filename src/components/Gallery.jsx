e458337f1134da8a4c4661d1a3e28458c637ad27
Megagig <megagigsoftwaresolutions.com>
2024-09-05 17:37:17 +0100

    Setup React Query

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,6 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import React from 'react';
 
+const url =
+  'https://api.unsplash.com/search/photos?client_id=1weEN284PoI2sHYX8Xf9VXprMqNNzii6hF0aAhh7Q8A&query=pharmacy';
+
 const Gallery = () => {
+  const response = useQuery({
+    queryKey: ['gallery'],
+    queryFn: async () => {
+      const res = await axios.get(url);
+      return res.data;
+    },
+  });
+  console.log(response);
   return (
     <div>
       <h2>Gallery</h2>
--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
 const SearchForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault;
+    const SearchValue = e.target.elements.search.value;
+    if (!SearchValue) return;
+    console.log(SearchValue);
+  };
   return (
-    <div>
-      <h2>SearchForm</h2>
-    </div>
+    <section>
+      <h1 className="title">Unsplsh Images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="cat"
+          className="form-input search-input"
+          name="search"
+        />
+        <button type="submit" className="btn">
+          Search
+        </button>
+      </form>
+    </section>
   );
 };
 
